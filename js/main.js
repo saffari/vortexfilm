e74936c66dd0dab6a8314809eeafb56a4664dcdd
mczegel <mate.czegel@gmail.com>
2013-05-17 17:22:17 +0200

    pagify

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$('#page_holder').pagify({ 
+	animation: 'slideDown',
+    pages: ['about', 'kolorado', 'tokyo', 'prick', 'youtube', 'contacts'], 
+    'default': 'kolorado' // The name of a page or null for an empty div
+}); 
